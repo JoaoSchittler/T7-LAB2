46f0e8910d03373983dcc332be80fddebdfc10fc
henb10 <45078469+henb10@users.noreply.github.com>
2018-11-22 19:21:07 -0200

    Update grafo.hpp

--- grafo.hpp
+++ grafo.hpp
@@ -1,1 +1,43 @@
+#ifndef GRAFO_HPP_INCLUDED
+#define GRAFO_HPP_INCLUDED
+
+#include<iostream>
+#include<list>
+#include<queue>
+#include<map>
+
+#include"geom.hpp"
+#include"Tela.hpp"
+
+using namespace geom;
+using namespace tela;
+
+enum COLOR{BRANCO, CINZA, PRETO};
+
+struct Vertice{
+    Ponto ponto;
+    Vertice* ant;
+    int dist;
+    COLOR cor;
+    std::list<Vertice*> adj;
+};
+
+struct Grafo{
+    Tela t;
+    std::map<int, Vertice> grafo;
+    std::map<int, Vertice>::iterator it;
+    std::map<int, Vertice>::iterator it_;
+
+    void busca_largura_lab(Vertice* saida, Vertice* entrada);
+
+    Vertice* busca_vertice(Ponto p);
+
+    void caminho_curto(Ponto fonte,Ponto destino);
+
+};
+
+
+
+
+#endif // GRAFO_HPP_INCLUDED
 
