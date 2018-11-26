e12574f3e46d2ba2aa28aec01064cbc40f3615c5
JoaoSchittler <45077941+JoaoSchittler@users.noreply.github.com>
2018-11-26 16:49:17 -0200

    Header do mapa.cpp
    -Criação da Struct Mapa e namespace mapa
    -Adicionadas as funções do mapa.cpp

--- mapa.hpp
+++ mapa.hpp
@@ -1,1 +1,18 @@
+#pragma once 
+#include "Tela.hpp"
+namespace mapa
+{
+   	struct Mapa
+   	{
+   		char** map;
+   		unsigned int lin;
+		  unsigned int col;
+		  //Função que cria um mapa a partir de um arquivo txt
+ 		  void cria(const char* nome_input);
+ 		  //Função de deleta a memória ocupada pelo mapa
+   	  void libera();
+   	  //Função que desenha o mapa na tela T
+		  void desenha(tela::Tela t);
 
+   	};
+};
