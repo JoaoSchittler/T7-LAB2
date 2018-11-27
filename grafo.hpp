310bf72a16847a5505d4e10a991cdf387fcf15fe
henb10 <45078469+henb10@users.noreply.github.com>
2018-11-27 00:40:34 -0200

    Update grafo.hpp

--- grafo.hpp
+++ grafo.hpp
@@ -1,43 +1,40 @@
-#ifndef GRAFO_HPP_INCLUDED
-#define GRAFO_HPP_INCLUDED
+#pragma once
 
 #include<iostream>
 #include<list>
 #include<queue>
 #include<map>
 
-#include"geom.hpp"
 #include"Tela.hpp"
 
-using namespace geom;
 using namespace tela;
 
-enum COLOR{BRANCO, CINZA, PRETO};
+namespace grafo{
 
-struct Vertice{
-    Ponto ponto;
-    Vertice* ant;
-    int dist;
-    COLOR cor;
-    std::list<Vertice*> adj;
-};
+    enum COLOR{BRANCO, CINZA, PRETO};
 
-struct Grafo{
-    Tela t;
-    std::map<int, Vertice> grafo;
-    std::map<int, Vertice>::iterator it;
-    std::map<int, Vertice>::iterator it_;
+    struct Vertice{
+        Ponto ponto;
+        Vertice* ant;
+        int dist;
+        COLOR cor;
+        std::list<Vertice*> adj;
+    };
 
-    void busca_largura_lab(Vertice* saida, Vertice* entrada);
+    struct Grafo{
+        Tela t;
+        std::map<int, Vertice> grafo;
+        std::map<int, Vertice>::iterator it;
+        std::map<int, Vertice>::iterator it_;
 
-    Vertice* busca_vertice(Ponto p);
+        void cria_lista_adj();
 
-    void caminho_curto(Ponto fonte,Ponto destino);
+        void busca_largura_lab(Vertice* saida, Vertice* entrada);
 
-};
+        Vertice* busca_vertice(Ponto p);
+
+        void caminho_curto(Ponto fonte,Ponto destino);
 
 
-
-
-#endif // GRAFO_HPP_INCLUDED
-
+    };
+}
