75ca50c33f41a46ecf8813fcf3665d42c175be9d
henb10 <45078469+henb10@users.noreply.github.com>
2018-11-27 00:42:37 -0200

    Adicionei namespace grafo e criei a funcao cria_lista_adj

--- grafo.cpp
+++ grafo.cpp
@@ -1,76 +1,89 @@
 #include"grafo.hpp"
 
+namespace grafo{
 
-///Busca largura com o algoritimo do jv
-void Grafo::busca_largura_lab(Vertice* saida, Vertice* entrada){
-    std::queue<Vertice*> fila;
-    fila.push(saida);
-    Vertice* aux;
-    it = grafo.begin();
-    while(it!=grafo.end()){
-        it->second.cor =  BRANCO;
-        it++;
-    }
-    while(fila.empty()==false){
-        aux = fila.front();
-        fila.pop();
-        for(Vertice* i : aux->adj){
-            if(i->cor == BRANCO){
-                i->cor = CINZA;
-                i->dist = aux->dist+1;
-                i->ant = aux;
-                fila.push(i);
+
+    void Grafo::cria_lista_adj(){
+        for(it = grafo.begin();it!=grafo.end();it++){
+
+            for(it_ = grafo.begin();it_!=grafo.end();it_++){
+
+                float dist_x = abs(it->second.ponto.x-it_->second.ponto.x);
+                float dist_y = abs(it->second.ponto.y-it_->second.ponto.y);
+                if((dist_x == 20 && dist_y == 0) || (dist_y == 20 && dist_x == 0)) {
+                    it->second.adj.push_back(&it_->second);
+                }
+
             }
+
         }
-        aux->cor = PRETO;
     }
-}
 
-
-///Essa parte eu ainda nao sei o que fazer diretio para achar o
-/// vertice desejado. Ainda nao mudei a chave do std::map para um Ponto
-Vertice* Grafo::busca_vertice(Ponto p){
-    it = grafo.begin();
-    while(it!=grafo.end()){
-        if(it->second.ponto.x == p.x
-           && it->second.ponto.y == p.y){
-            return &it->second;
+    void Grafo::busca_largura_lab(Vertice* saida, Vertice* entrada){
+        std::queue<Vertice*> fila;
+        fila.push(saida);
+        Vertice* aux;
+        it = grafo.begin();
+        while(it!=grafo.end()){
+            it->second.cor =  BRANCO;
+            it++;
         }
-        it++;
-    }
-    return nullptr;
-}
-
-
-///Faz o caminho mais curto entre dois pontos
-///e desenha as bolinhas azuis nos vertices que é para passar
-///os if para ver se é nullptr da para retirar(eu acho)
-/// é só passar um ponto que seja um vertice sempre
-void Grafo::caminho_curto(Ponto fonte,Ponto destino){
-    Vertice* saida = busca_vertice(fonte);
-    if(saida == nullptr){
-        std::cout<<"Saida eh invalido";
-        return;
-    }
-    Vertice* entrada = busca_vertice(destino);
-    if(entrada == nullptr){
-        std::cout<<"Entrada eh invalido";
-        return;
+        while(fila.empty()==false){
+            aux = fila.front();
+            fila.pop();
+            for(Vertice* i : aux->adj){
+                if(i->cor == BRANCO){
+                    i->cor = CINZA;
+                    i->dist = aux->dist+1;
+                    i->ant = aux;
+                    fila.push(i);
+                }
+            }
+            aux->cor = PRETO;
+        }
     }
 
-    if(entrada == saida){
-        Circulo c = {{saida->ponto.x,saida->ponto.y}, 4};
-        t.cor({0.1, 0.1, 0.9});
-        t.circulo(c);
-        return;
+
+
+    Vertice* Grafo::busca_vertice(Ponto p){
+        it = grafo.begin();
+        while(it!=grafo.end()){
+            if(it->second.ponto.x == p.x
+               && it->second.ponto.y == p.y){
+                return &it->second;
+            }
+            it++;
+        }
+        return nullptr;
     }
-    ///No jogo do pacman sempre ha um caminho
-    if(entrada->ant == NULL){
-        std::cout<<"Nao existe caminho \n";
-    } else {
-        caminho_curto(fonte, entrada->ant->ponto );
-        Circulo c = {{entrada->ponto.x,entrada->ponto.y}, 4};
-        t.cor({0.1, 0.1, 0.9});
-        t.circulo(c);
+
+
+
+    void Grafo::caminho_curto(Ponto fonte,Ponto destino){
+        Vertice* saida = busca_vertice(fonte);
+        if(saida == nullptr){
+            std::cout<<"Saida eh invalido";
+            return;
+        }
+        Vertice* entrada = busca_vertice(destino);
+        if(entrada == nullptr){
+            std::cout<<"Entrada eh invalido";
+            return;
+        }
+
+        if(entrada == saida){
+            Circulo c = {{saida->ponto.x,saida->ponto.y}, 4};
+            t.circulo(c, {0.1, 0.1, 0.9});
+            return;
+        }
+        if(entrada->ant == NULL){
+            std::cout<<"Nao existe caminho \n";
+        } else {
+            caminho_curto(fonte, entrada->ant->ponto );
+            Circulo c = {{entrada->ponto.x,entrada->ponto.y}, 4};
+            t.circulo(c, {0.1, 0.1, 0.9});
+        }
     }
+
+
 }
