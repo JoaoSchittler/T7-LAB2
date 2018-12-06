cbf50c08e2ae64ee597a2d8e9295639c3973c6af
JoaoSchittler <45077941+JoaoSchittler@users.noreply.github.com>
2018-12-06 12:05:05 -0200

    Atualização mapa.hpp 2

--- mapa.hpp
+++ mapa.hpp
@@ -6,13 +6,15 @@ namespace mapa
    	{
    		char** map;
    		unsigned int lin;
-		  unsigned int col;
-		  //Função que cria um mapa a partir de um arquivo txt
- 		  void cria(const char* nome_input);
- 		  //Função de deleta a memória ocupada pelo mapa
-   	  void libera();
-   	  //Função que desenha o mapa na tela T
-		  void desenha(tela::Tela t);
+		unsigned int col;
+		float           xini;
+		float	        yini;
+		//Função que cria um mapa a partir de um arquivo txt
+ 		void cria(const char* nome_input);
+ 		//Função de deleta a memória ocupada pelo mapa
+   	        void libera();
+   	        //Função que desenha o mapa na tela T
+		void desenha(tela::Tela t);
 
    	};
 };
